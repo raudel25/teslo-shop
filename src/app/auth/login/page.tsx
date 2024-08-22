9f349bca148bb63474493932ada4cc7145b4d483
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2024-08-22 01:51:03 -0400

    first commit

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <main className="">
+      <h1>Login</h1>
+    </main>
+  );
+}
---
+++ src/app/auth/new-account/page.tsx
@@ -0,0 +1,7 @@
+export default function NewAccount() {
+  return (
+    <main className="">
+      <h1>NewAccount</h1>
+    </main>
+  );
+}
