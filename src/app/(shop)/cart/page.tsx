47c688e5d8a71cf4fe8e872fdfdaaf10cfb31e56
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2024-08-26 23:36:56 -0400

    order summary

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,15 +1,7 @@
-import { QuantitySelector } from "@/components";
 import Title from "@/components/ui/title/Title";
-import { initialData } from "@/seed/seed";
-import Image from "next/image";
 import Link from "next/link";
 import { ProductsInCart } from "./ui/ProductsInCart";
-
-const productsInCar = [
-  initialData.products[0],
-  initialData.products[1],
-  initialData.products[2],
-];
+import { OrderSummary } from "@/components";
 
 export default function CartPage() {
   return (
@@ -28,22 +20,7 @@ export default function CartPage() {
           </div>
 
           <div className="bg-white rounded-xl shadow-xl p-7 h-fit">
-            <h2 className="text-2xl mb-2">Order summary</h2>
-
-            <div className="grid grid-cols-2">
-              <span>No. Products</span>
-              <span className="text-right">3 articles</span>
-
-              <span>Subtotal</span>
-              <span className="text-right">$ 100</span>
-
-              <span>Impuestos</span>
-              <span className="text-right">$ 100</span>
-
-              <span className="mt-5 text-2xl">Total:</span>
-              <span className="mt-5 text-2xl text-right">$ 100</span>
-            </div>
-
+            <OrderSummary />
             <div className="mt-5 mb-2 w-full">
               <Link
                 className="flex btn-primary justify-center"
--- src/components/order/summary/OrderSummary.tsx
+++ src/components/order/summary/OrderSummary.tsx
@@ -1,20 +1,52 @@
+"use client";
+
+import { cartStore } from "@/store";
+import { currencyFormat } from "@/utils";
+
 export const OrderSummary = () => {
+  const { products } = cartStore();
   return (
     <div>
       <h2 className="text-2xl mb-2">Order summary</h2>
 
       <div className="grid grid-cols-2">
         <span>No. Products</span>
-        <span className="text-right">3 articles</span>
+        <span className="text-right">
+          {products.reduce((total, item) => item.quantity + total, 0)}{" "}
+          {products.reduce((total, item) => item.quantity + total, 0) === 1
+            ? "article"
+            : "articles"}
+        </span>
 
         <span>Subtotal</span>
-        <span className="text-right">$ 100</span>
+        <span className="text-right">
+          {currencyFormat(
+            products.reduce(
+              (price, item) => item.quantity * item.price + price,
+              0
+            )
+          )}
+        </span>
 
-        <span>Impuestos</span>
-        <span className="text-right">$ 100</span>
+        <span>Taxes (15%)</span>
+        <span className="text-right">
+          {currencyFormat(
+            products.reduce(
+              (price, item) => item.quantity * item.price + price,
+              0
+            ) * 0.15
+          )}
+        </span>
 
         <span className="mt-5 text-2xl">Total:</span>
-        <span className="mt-5 text-2xl text-right">$ 100</span>
+        <span className="mt-5 text-2xl text-right">
+          {currencyFormat(
+            products.reduce(
+              (price, item) => item.quantity * item.price + price,
+              0
+            ) * 1.15
+          )}
+        </span>
       </div>
     </div>
   );
